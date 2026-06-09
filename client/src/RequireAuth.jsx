7659af531c693b4a68d7129f16fa93eae317265f
SANJANA_PULLI <sanjanapulli24@gmail.com>
2026-06-09 14:52:53 +0530

    Update RequireAuth component

--- client/src/RequireAuth.jsx
+++ client/src/RequireAuth.jsx
@@ -1,8 +1,8 @@
 import { Navigate } from 'react-router-dom'
 
 export default function RequireAuth({ children, role }) {
-  const admin = localStorage.getItem('adminToken')
-  const employee = localStorage.getItem('employeeToken')
+  const admin = localStorage.getItem('adminAuth')
+  const employee = localStorage.getItem('employeeAuth')
 
   if (role === 'admin' && !admin) {
     return <Navigate to="/login" replace />
