b45e03141db24afced1e578c8687017cc892a09e
SANJANA_PULLI <sanjanapulli24@gmail.com>
2026-06-09 14:16:08 +0530

    Fix protected routes and add RequireAuth for admin and employee

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,33 @@
 import { Routes, Route } from 'react-router-dom'
+
 import ScanPage from './pages/ScanPage'
 import AdminLogin from './pages/AdminLogin'
+import EmployeeLogin from './pages/EmployeeLogin'
+
 import Dashboard from './pages/Dashboard'
 import Employees from './pages/Employees'
 import Attendance from './pages/Attendance'
 import AdminLeave from './pages/AdminLeave'
-import EmployeeLeave from './pages/EmployeeLeave'
-import EmployeeLayout from './layouts/EmployeeLayout'
-import EmployeeDashboard from './pages/EmployeeDashboard'
-import AdminLayout from './layouts/AdminLayout'
-import EmployeeLogin from './pages/EmployeeLogin'
-import EmployeeProfile from './pages/EmployeeProfile'
-import EmployeeAttendance from './pages/EmployeeAttendance'
-import TeamScan from './pages/TeamScan'
-import ScannerTerminal from './pages/ScannerTerminal'
 import Announcements from './pages/Announcements'
 import Reports from './pages/Reports'
-import Settings from './pages/Settings'
-import ScrollToTop from './components/ScrollToTop'
 import Activity from './pages/Activity'
+import Settings from './pages/Settings'
 import AdminTask from './pages/AdminTask'
+
+import EmployeeDashboard from './pages/EmployeeDashboard'
+import EmployeeLeave from './pages/EmployeeLeave'
+import EmployeeProfile from './pages/EmployeeProfile'
+import EmployeeAttendance from './pages/EmployeeAttendance'
 import EmployeeTask from './pages/EmployeeTask'
+
+import TeamScan from './pages/TeamScan'
+import ScannerTerminal from './pages/ScannerTerminal'
+
+import AdminLayout from './layouts/AdminLayout'
+import EmployeeLayout from './layouts/EmployeeLayout'
+
+import ScrollToTop from './components/ScrollToTop'
+import RequireAuth from './RequireAuth'
 
 function App() {
   return (
@@ -28,11 +35,22 @@ function App() {
       <ScrollToTop />
 
       <Routes>
+
+        {/* PUBLIC ROUTES */}
         <Route path="/" element={<ScanPage />} />
         <Route path="/login" element={<AdminLogin />} />
         <Route path="/employee/login" element={<EmployeeLogin />} />
+        <Route path="/scan" element={<ScannerTerminal />} />
 
-        <Route path="/admin" element={<AdminLayout />}>
+        {/* 🔐 ADMIN ROUTES (PROTECTED) */}
+        <Route
+          path="/admin"
+          element={
+            <RequireAuth role="admin">
+              <AdminLayout />
+            </RequireAuth>
+          }
+        >
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="employees" element={<Employees />} />
           <Route path="attendance" element={<Attendance />} />
@@ -44,7 +62,15 @@ function App() {
           <Route path="tasks" element={<AdminTask />} />
         </Route>
 
-        <Route path="/employee" element={<EmployeeLayout />}>
+        {/* 🔐 EMPLOYEE ROUTES (PROTECTED) */}
+        <Route
+          path="/employee"
+          element={
+            <RequireAuth role="employee">
+              <EmployeeLayout />
+            </RequireAuth>
+          }
+        >
           <Route path="dashboard" element={<EmployeeDashboard />} />
           <Route path="leave" element={<EmployeeLeave />} />
           <Route path="profile" element={<EmployeeProfile />} />
@@ -54,7 +80,6 @@ function App() {
           <Route path="tasks" element={<EmployeeTask />} />
         </Route>
 
-        <Route path="/scan" element={<ScannerTerminal />} />
       </Routes>
     </>
   )
